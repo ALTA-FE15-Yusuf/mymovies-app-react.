49fe370592cdb294ca7ae219c952b709ff48763d
YusufPandego253 <138494122+YusufPandego253@users.noreply.github.com>
2023-08-22 17:26:48 +0700

    Update: Added Fetch API

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { Component } from 'react'
 import Card from './components/Card'
 import Navbar from './components/Navbar'
 import "./App.css"
-import data from "../src/dummy/Movie.json"
 import Detail from './components/Detail'
 import Favorite from './components/Favorite'
+import axios from 'axios'
 
 
 
 interface DarkModeState {
   mode: boolean,
   popup: boolean,
-  count: number
+  count: number,
+  data:any,
+  Authentication:any,
 }
 
 class App extends Component<DarkModeState> {
@@ -19,7 +21,8 @@ class App extends Component<DarkModeState> {
   state = {
     mode: false,
     popup: false,
-    count: 0
+    count: 0,
+    data: []
   }
 
   changeMode() {
@@ -29,9 +32,27 @@ class App extends Component<DarkModeState> {
   showPopup() {
     this.setState({ popup: true })
   }
+  getMovie() {
+    axios.get('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',{
+      headers:{
+        'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YWIyMGZjNGJmZTJlYTI4MWVlOGQwMjM0MzZjNjljZSIsInN1YiI6IjY0ZGY4MDk5ZDEwMGI2MTRiNGNkNzcwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Zr6K9FOHVjzPBqPMiIyd_d_EepaUu3TbDryCyBfSqrI`
+      }
+    })
+        .then((response) => {
+            this.setState({ data: response?.data?.results })
+            console.log("data")
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+componentDidMount(): void {
+    this.getMovie()
+}
 
   render() {
-    const { mode} = this.state
+    const { mode, data} = this.state
     return (
       <section className={`${mode === true ? "bg-black" : "bg-white"}`}>
         <div className='BG'>
@@ -42,15 +63,15 @@ class App extends Component<DarkModeState> {
 
         <div className='flex flex-wrap item-center mx-2 my-5 gap-y-5 gap-x-5'>
           {
-            data.map((item, index) => {
+            data && 
+            data?.map((item:any, index) => {
               return (
                 <div className='mx-5'>
                   <Card
                     key={index}
-                    id="Movie"
-                    image={item.image}
-                    title={item.title}
-                    price={item.price}
+                    id={item.id.toString()}
+                    poster_path={item?.poster_path}
+                    title={item?.title}
                     type={mode === true ? "dark" : "light"}
                     onClick={() => alert('Added to Favorite')}
                   />
@@ -59,7 +80,7 @@ class App extends Component<DarkModeState> {
             })
           }
         </div>
-        <div className='my-5 grid grid-cols-1 ms-5 space-y-5'>
+        <div className='my-5 grid grid-cols-1 ms-5'>
           <Detail 
           type={mode === true ? "dark" : "light"}/>
         </div>
